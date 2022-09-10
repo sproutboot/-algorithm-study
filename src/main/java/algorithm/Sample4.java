1b8bb1436a87d729d5e2cce719ec5555da70442e
sproutboot <serverspring@naver.com>
2022-09-11 04:11:29 +0900

    숫자만 추출 - 문자열을 문자 배열로 만들어서 단어를 하나하나 비교하면서 숫자만 추출

---
+++ src/main/java/algorithm/Sample4.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Sample4 {
+
+    public int solution(String str) {
+        String answer = "";
+
+        for(char x : str.toCharArray()) {
+            if(Character.isDigit(x)) answer += x;
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        Sample4 sample4 = new Sample4();
+
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(sample4.solution(str));
+    }
+}
---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void 계산기_테스트() {
+        Calculator calculator = new Calculator();
+
+
+    }
+
+
+}
