2cd4e1aebafd5e15c7e98d4821b3b98d2917066f
sproutboot <serverspring@naver.com>
2022-09-10 11:08:31 +0900

    팰린드롬 - 특수문자, 알파벳, 공백이 있을 때 replaceAll() 정규식으로 알파벳만 추출해서 로직 구현 - StringBuilder.reverse()를 이용

---
+++ src/main/java/algorithm/Sample3.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Sample3 {
+
+    public String solution(String str) {
+        String answer = "NO";
+
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+
+        String tmp = new StringBuilder(str).reverse().toString();
+
+        if(str.equals(tmp)) answer = "YES";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sample3 sample = new Sample3();
+
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.print(sample.solution(str));
+    }
+}
