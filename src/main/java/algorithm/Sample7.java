c13f561a54f21a74125a0ca83b095314c0931f5e
sproutboot <serverspring@naver.com>
2022-09-13 16:19:22 +0900

    암호

---
+++ src/main/java/algorithm/Sample7.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Sample7 {
+
+    public String solution(int n, String str) {
+        String answer = "";
+
+        for(int i = 0; i < n; i++) {
+            String tmp = str.substring(0,7).replace('#','1').replace('*','0');
+            int num = Integer.parseInt(tmp, 2);
+            answer+= (char)num;
+            str = str.substring(7);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sample7 sample = new Sample7();
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String str = kb.next();
+
+        System.out.println(sample.solution(n,str));
+    }
+}
