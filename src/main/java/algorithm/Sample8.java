441026115919445fe26a03f689939df6776f5297
sproutboot <serverspring@naver.com>
2022-09-14 10:51:32 +0900

    문자 찾기 - 대소문자 변환 toUpperCase()

---
+++ src/main/java/algorithm/Sample8.java
@@ -0,0 +1,28 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Sample8 {
+
+    public int solution(String str, char c) {
+        int answer = 0;
+
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == c) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sample8 sample = new Sample8();
+
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        System.out.print(sample.solution(str,c));
+    }
+}
