d1e9a3519c07f295c007663ad5663f13d23213d2
sproutboot <serverspring@naver.com>
2022-09-25 08:57:54 +0900

    TreeSet

---
+++ src/main/java/algorithm/hash/Sample4.java
@@ -0,0 +1,42 @@
+package algorithm.hash;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Sample4 {
+
+    public int solution(int n, int k, int[] arr) {
+        int answer = -1;
+        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                for(int l = j+1; l < n; l++) {
+                    treeSet.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        for(int x : treeSet) {
+            cnt++;
+            if(cnt == k) return x;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sample4 sample4 = new Sample4();
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.print(sample4.solution(n, k, arr));
+    }
+}
