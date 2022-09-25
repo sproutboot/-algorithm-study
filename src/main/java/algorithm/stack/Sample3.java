af6547b6a080d7c321c6e25d27956ad82c12256c
sproutboot <serverspring@naver.com>
2022-09-26 08:42:56 +0900

    Stack

---
+++ src/main/java/algorithm/stack/Sample3.java
@@ -0,0 +1,32 @@
+package algorithm.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Sample3 {
+
+    public String solution(String str) {
+        String answer = " ";
+        Stack<Character> stack = new Stack<>();
+
+        for(char x : str.toCharArray()) {
+            if(x == ')') {
+                while(stack.pop() != '(');
+            }
+            else stack.push(x);
+        }
+
+        for(int i = 0; i < stack.size(); i++) {
+            answer+= stack.get(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sample3 sample3 = new Sample3();
+
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(sample3.solution(str));
+    }
+}
