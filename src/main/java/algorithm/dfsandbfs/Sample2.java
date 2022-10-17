db795422e97e690b85dd27a4c1259f24aceba3bb
sproutboot <serverspring@naver.com>
2022-10-17 10:34:14 +0900

    DFS

---
+++ src/main/java/algorithm/dfsandbfs/Sample2.java
@@ -0,0 +1,42 @@
+package algorithm.dfsandbfs;
+
+import java.util.Scanner;
+
+public class Sample2 {
+
+    static String answer = "NO";
+    static int n, total = 0;
+    boolean flag = false;
+
+    public void DFS(int L, int sum, int[] arr) {
+        if(flag) return;
+        if(sum > total / 2) return;
+        if(L == n) {
+            if((total - sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+        }
+        else {
+            DFS(L+1, sum + arr[L], arr);
+            DFS(L+1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Sample2 sample2 = new Sample2();
+        Scanner kb = new Scanner(System.in);
+
+        n = kb.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+            total += arr[i];
+        }
+
+        sample2.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+
+}
