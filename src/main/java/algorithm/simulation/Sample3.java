0dce5b9afd6b4969d6f16c10a5880c723cd0b518
sproutboot <serverspring@naver.com>
2022-12-01 20:14:22 +0900

    구현

--- src/main/java/algorithm/simulation/Sample3.java
+++ src/main/java/algorithm/simulation/Sample3.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Sample4 {
+public class Sample3 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String str = br.readLine();
         StringTokenizer st = new StringTokenizer(str, " ");
         int H = Integer.parseInt(st.nextToken());
